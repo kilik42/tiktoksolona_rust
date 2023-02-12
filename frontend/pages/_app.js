fc87c63641487210c6f6c496fc09f108c8dc8dbd
kilik42 <maevins@cps.edu>
2023-02-11 20:25:39 -0600

    working on wallet signinn

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import  dynamic from 'next/dynamic';
+import '@solana/wallet-adaptter-react-ui/styles.css';
 import { useEffect, useState } from 'react';
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletConnectProvider } from "@solana/wallet-adapter-walletconnect";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const WalletConnectionProvider = dynamic(
+    () => import('../context/walletConnectProvider'),
+    { ssr: false },
+  )
+  return (
+    <WalletConnectionProvider>
+      <Component {...pageProps} />
+    </WalletConnectionProvider>
+  )
 }
 
 export default MyApp
