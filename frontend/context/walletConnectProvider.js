a4653fbe0d0c8597ad7d738f23ac4e728cca028a
kilik42 <maevins@cps.edu>
2023-02-11 20:16:33 -0600

    working on wallet signinn

--- frontend/context/walletConnectProvider.js
+++ frontend/context/walletConnectProvider.js
@@ -4,6 +4,7 @@ import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { getPhantomWallet } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import React from "react";
+import {useMemo} from "react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { WalletDisconnectButton } from "@solana/wallet-adapter-react-ui";
@@ -11,21 +12,39 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { useConnection } from "@solana/wallet-adapter-react";
 import { useWalletModal } from "@solana/wallet-adapter-react-ui";
 
-const MainView = () => {
-    const wallet = useWallet();
-    const connection = useConnection();
-    const { setVisible } = useWalletModal();
-    
-    return (
-        <div className="mainView">
-        <div className="walletInfo">
-            <div className="walletName">{wallet.name}</div>
-            <div className="walletAddress">{wallet.publicKey?.toBase58()}</div>
-        </div>
-        <div className="walletButtons">
-            <WalletMultiButton />
-            <WalletDisconnectButton />
-        </div>
-        </div>
+// const network = clusterApiUrl("devnet");
+// const endpoint = network;
+
+
+const walletConnectProvider = ({children}) => {
+    const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+    const wallets = useMemo(() => [ new PhantomWalletAdapter(), new WalletConnectProvider({ rpc: { devnet: endpoint } }) ], []
     );
-    }
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>{children}</WalletModalProvider>
+        </WalletProvider>
+        </ConnectionProvider>
+    );
+}
+export default walletConnectProvider;
+
+// const MainView = () => {
+//     const wallet = useWallet();
+//     const connection = useConnection();
+//     const { setVisible } = useWalletModal();
+    
+//     return (
+//         <div className="mainView">
+//         <div className="walletInfo">
+//             <div className="walletName">{wallet.name}</div>
+//             <div className="walletAddress">{wallet.publicKey?.toBase58()}</div>
+//         </div>
+//         <div className="walletButtons">
+//             <WalletMultiButton />
+//             <WalletDisconnectButton />
+//         </div>
+//         </div>
+//     );
+//     }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css'
+import  dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletConnectProvider } from "@solana/wallet-adapter-walletconnect";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
